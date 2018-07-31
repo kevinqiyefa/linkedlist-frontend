771702d79d375048f7d0dad2f5a711d82764179a
Matt Lane <lane.matthew@gmail.com>
2018-07-31 16:56:26 -0700

    header finished except for dropdown

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,72 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+const DEFAULT_STATE = {
+  searchText: '',
+  searchCategoryIdx: 0,
+  isDropdownVisible: false
+};
 
 export default class Header extends Component {
+  state = DEFAULT_STATE;
+
+  handleSearch = e => {
+    e.preventDefault();
+    // TODO: search
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleClick = idx => {
+    this.setState({ searchCategoryIdx: idx });
+  };
+
   render() {
+    const { searchText, searchCategoryIdx } = this.state;
+    const { searchCategories, displayName, profilePic } = this.props;
     return (
-      <div>
-        header placeholder
-        {/* Home Button */}
-        {/* Search Form */}
-        {/* Search Dropdown */}
-        {/* Current User Photo & DropDown */}
-        {this.props.cool && <h1> cool af </h1>}
+      <div className="Header">
+        <Link to="/" className="Header-logo">
+          LL
+        </Link>
+        <form className="search-form" onSubmit={this.handleSearch}>
+          <div className="search">
+            <span className="fas fa-search" />
+            <input
+              type="text"
+              name="searchText"
+              placeholder="Search for Companies, Jobs, or People"
+              onChange={this.handleChange}
+              value={searchText}
+            />
+          </div>
+          <div className="search-categories">
+            {searchCategories.map((category, i) => (
+              <div key={category}>
+                <input
+                  type="radio"
+                  id={category}
+                  checked={i === searchCategoryIdx}
+                  onChange={this.handleClick.bind(this, i)}
+                />
+                <label htmlFor={category}>{category}</label>
+              </div>
+            ))}
+          </div>
+          <input type="submit" value="Search" className="search-btn" />
+        </form>
+        <div className="profile-area">
+          <img src={profilePic} />
+          <span>{displayName}</span>
+        </div>
       </div>
     );
   }
 }
+
+Header.defaultProps = {
+  searchCategories: ['companies', 'users', 'jobs']
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'notosans-fontface/css/notosans-fontface.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
